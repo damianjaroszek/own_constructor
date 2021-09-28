de3799235bda60b06e872d9be0cdcb7bd9b1bcd7
unknown <deejay.8lm@gmail.com>
2021-09-28 20:59:54 +0200

    optymalizacja kodu poprzez przeniesienie metody sayHello z klasy Person do prototype oraz przeciazenie metody sayHello na person1

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,9 @@ this.lName = lastName;
 
 }
 
-Person.prototype.sayHello = function(){
+Person.prototype.sayHello = function(){ // oprymalizacja kodu poprzez przeniesienie metody sayHello z 
+                                        // klasy Person do prototype - funkcja nie zapycha miejsca, bo 
+                                        // nie tworzy się za każdym razem gdy tworzony jest obiekt
     return this.fName + " " + this.lName;
 };
 
@@ -12,6 +14,6 @@ Person.prototype.sayHello = function(){
 var person1 = new Person ("Jan", "Kowalski");
 var person2 = new Person ("Tomasz", "Nowak");
 
-person1.sayHello = function(){
+person1.sayHello = function(){ //przeciążenie metody sayHello
     return this.fName.toUpperCase() + " " + this.lName.toUpperCase();    
 };
